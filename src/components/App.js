d4e39580494518f0ed6615138ec36b9fcd92b26c
Jose Fletes <joseangelfletes@gmail.com>
2022-08-11 11:42:36 -0700

    commit tree 1f00225678c60b7b11f8f1b46904a7bc64ade270

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings] = useState([])
+  const [search, setSearch] = useState("")
+
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(response => response.json())
+      .then(listings => setListings(listings))
+  }, [])
+
+  const handleDeleteListing = (deletedItem) => {
+    const updatedListings = listings.filter(listing => listing.id !== deletedItem)
+    setListings(updatedListings)
+  }
+
+  const displayedListing = listings.filter(listing => listing.description.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header
+        onSearch={setSearch}
+      />
+      <ListingsContainer
+        listings={displayedListing}
+        onDeleteListing={handleDeleteListing}
+      />
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Search() {
+function Search({ onSearch }) {
+  const [userInput, setUserInput] = useState("")
+
   function handleSubmit(e) {
     e.preventDefault();
-    console.log("submitted");
+    onSearch(userInput);
   }
 
   return (
@@ -12,8 +14,8 @@ function Search() {
         type="text"
         id="search"
         placeholder="search free stuff"
-        value={""}
-        onChange={(e) => console.log(e.target.value)}
+        value={userInput}
+        onChange={(e) => setUserInput(e.target.value)}
       />
       <button type="submit">🔍</button>
     </form>
